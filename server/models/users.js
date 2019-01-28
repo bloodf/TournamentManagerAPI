3e0ba078c5eaf21bcf99e611fa8ad8043b3ac23e
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2019-01-28 19:45:01 -0200

    Adicionado Senha ao usuário

--- server/models/users.js
+++ server/models/users.js
@@ -1,13 +1,38 @@
+/*eslint-disable */
+const bcrypt = require('bcrypt');
+
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('Users', {
     name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    dci: DataTypes.STRING,
+    email: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
+    password: DataTypes.STRING,
+    dci: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
     phone: DataTypes.STRING,
-    deviceId: DataTypes.STRING,
-    facebookId: DataTypes.STRING,
+    facebookId: {
+      type: DataTypes.STRING,
+      unique: true,
+    },
     active: DataTypes.BOOLEAN,
-  }, {});
+  }, {
+    hooks: {
+      async beforeCreate(user) {
+        user.password = await bcrypt.hash(`${user.password}`, 10);
+      },
+      async beforeUpdate(user) {
+        user.password = await bcrypt.hash(`${user.password}`, 10);
+      },
+    },
+  });
+
+  User.prototype.validPassword = async function (password) {
+    return bcrypt.compare(`${password}`, `${this.password}`);
+  };
 
   User.associate = (models) => {
 
